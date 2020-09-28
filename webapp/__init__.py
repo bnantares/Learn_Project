fa0a253128f588d7d43de22e3c00f1a0dbb85b9e
Mikhail Knyazev <knyazevmo@gmail.com>
2020-09-28 13:10:33 +0300

    каркас

---
+++ webapp/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Villas')
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html', title='Registration')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
