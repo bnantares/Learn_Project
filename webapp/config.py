76307f959e35676044a3f995fb415d0b05fdfcd3
Mikhail Knyazev <knyazevmo@gmail.com>
2020-10-15 22:41:45 +0300

    Работа с шаблонами, сделал формы регистрации и авторизации, настроил БД и сделал миграции, Blueprint

--- webapp/config.py
+++ webapp/config.py
@@ -1,7 +1,11 @@
+from datetime import timedelta
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__)) #абсолютный путь до этой папки
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+
+REMEMBER_COOKIE_DURATION = timedelta(days=10)
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-#print(SQLALCHEMY_DATABASE_URI)
+SECRET_KEY = 'fr$jhfhr$jvsjvnln@3km$kgmfs'
