616a6be6ecfc3cd4790f16adca0e5c51460d6489
Anton Petrov <ant8151@yandex.ru>
2020-10-19 16:38:23 +0300

    What about init file

---
+++ __init__.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for, request
+from webapp.model import db, Users, Objects
+
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+    @app.route('/index')
+    @app.route('/')
+    def index():
+        return render_template('index.html', title='Real Estate Selling project by M.Knyazev & A.Petrov')
+
+    @app.route('/registration')
+    def registration():
+        return render_template('registration.html', title='Registration')
+    return app
+
+
+#set FLASK_APP=webapp && set FLASK_ENV=development && set FLASK_DEBUG=1 && flask run
+#if __name__ == '__main__':
+#    app.run(debug=True)
