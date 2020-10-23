534e97913d9f6f9f53a29e52bc6d99d935d431a7
Anton Petrov <ant8151@yandex.ru>
2020-10-23 15:49:27 +0300

    Добавлена страница с лотами

---
+++ webapp/object/views.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+blueprint = Blueprint('lots', __name__, url_prefix='/lots')
+
+@blueprint.route('/')
+def lots():
+    page_title = "Лоты на продажу"
+    return render_template('object/lots.html', page_title=page_title)
